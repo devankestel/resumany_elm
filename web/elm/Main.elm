00b4e19294b5631af6dbd68b3d2a2ac7b4260c57
Devan Kestel <devan.kestel@gmail.com>
2017-01-03 19:17:47 -0500

    We have a counter.
    Even though that’s not what we were going for.

--- web/elm/Main.elm
+++ web/elm/Main.elm
@@ -1,6 +1,32 @@
-module Main exposing(..)
-
-import Html exposing (text)
+module Main exposing (..)
+import Html exposing (..)
+import Html.Events exposing (onClick)
 
 main =
-  text "Hello from the Elllllllllm."
+  Html.beginnerProgram { model = model, view = view, update = update }
+
+type alias Model = Int
+
+model: Model
+model =
+  0
+
+type Msg = Increment | Decrement
+
+update: Msg -> Model -> Model
+
+update msg model =
+  case msg of
+    Increment ->
+      model + 1
+    Decrement ->
+      model - 1
+
+view : Model -> Html Msg
+
+view model =
+  div []
+    [ button [ onClick Decrement ] [ text "-" ]
+      , div [] [ text (toString model) ]
+      , button [ onClick Increment ] [ text "+" ]
+    ]
